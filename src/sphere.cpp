f659a509fc67ab3fb7362e337edf1494fd17a68d
Keven Villeneuve <kev.v@hotmail.com>
2017-05-25 16:23:16 -0400

    Fix bug in Ray-Sphere intersect

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -10,6 +10,7 @@ NORI_NAMESPACE_BEGIN
 class Sphere : public Shape {
 public:
 	Sphere(const PropertyList &propList):
+		m_center(propList.getPoint("center", Point3f(0.0f))),
 		m_radius(propList.getFloat("radius", 1.0f)),
 		m_zMin(propList.getFloat("zMin", -m_radius)),
 		m_zMax(propList.getFloat("zMax", m_radius)),
@@ -17,9 +18,8 @@ public:
 		m_thetaMax(propList.getFloat("thetaMax", M_PI)),
 		phiMax(propList.getFloat("phiMax", 2*M_PI))
 	{
-		m_bbox.expandBy(nori::BoundingBox3f(
-			nori::Point3f(-m_radius, -m_radius, m_zMin),
-			nori::Point3f(m_radius, m_radius, m_zMax)));
+		m_bbox.expandBy(m_center - Vector3f(m_radius));
+		m_bbox.expandBy(m_center + Vector3f(m_radius));
 	}
 
 	virtual uint32_t getPrimitiveCount() const override {
@@ -31,7 +31,7 @@ public:
 	}
 
 	virtual Point3f getCentroid(uint32_t index) const override {
-		return m_bbox.getCenter();
+		return m_center;
 	}
 
 	virtual bool rayIntersect(uint32_t index, const Ray3f &ray, float &u, float &v, float &t) const override {
@@ -39,8 +39,8 @@ public:
 
 		// compute quadratic sphere coefficients
 		float a = ray.d.dot(ray.d);
-		float b = 2.0f * ray.d.dot(ray.o - nori::Point3f(0.0f,0.0f,1.0f));
-		float c = (ray.o - nori::Point3f(0.0f, 0.0f, 1.0f)).dot(ray.o - nori::Point3f(0.0f, 0.0f, 1.0f)) - m_radius * m_radius;
+		float b = 2.0f * ray.d.dot(ray.o - m_center);
+		float c = (ray.o - m_center).dot(ray.o - m_center) - m_radius * m_radius;
 
 		// t = solve quadratic equation
 		float t0, t1;
@@ -101,7 +101,7 @@ public:
 		rayIntersect(index, ray, u, v, t);
 		its.p = ray(t);
 
-		Vector3f l = its.p - nori::Point3f(0.0f,0.0f,1.0f);
+		Vector3f l = its.p - m_center;
 		Vector3f n = l.normalized();
 		its.geoFrame = Frame(n);
 		its.shFrame = Frame(n);
@@ -111,14 +111,17 @@ public:
 		return tfm::format(
 			"%s\n"
 			"Sphere[\n"
+			"center = %s"
 			"radius = %d"
 			"]",
 			Shape::toString(),
+			m_center.toString(),
 			m_radius
 		);
 	}
 
 protected:
+	Point3f m_center;
 	float m_radius;
 	float m_zMin, m_zMax;
 	float m_thetaMin, m_thetaMax, phiMax;
