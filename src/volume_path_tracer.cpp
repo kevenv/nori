c717cf7307636962882769f9b91bc419b1530a53
Keven Villeneuve <keven.villeneuve@gmail.com>
2017-10-03 16:18:32 -0400

    VPT : Very basic Volumetric Path Tracer

---
+++ src/volume_path_tracer.cpp
@@ -0,0 +1,152 @@
+
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/warp.h>
+#include <nori/shape.h>
+#include <nori/bsdf.h>
+#include <nori/emitter.h>
+#include <nori/direct.h>
+
+NORI_NAMESPACE_BEGIN
+
+class VolumePT : public Integrator {
+public:
+    VolumePT(const PropertyList &props) :
+        m_tracerType(props.getString("tracerType", "explicit")),
+        m_termination(props.getString("termination", "russian-roulette")),
+        m_terminationProb(props.getFloat("terminationProb", 0.2f)),
+        m_terminationBounds(props.getInteger("terminationBounds", 15)),
+        m_sigmaA(props.getFloat("sigmaA", 0.1f)),
+        m_sigmaS(props.getFloat("sigmaS", 0.5f)),
+        m_sigmaT(m_sigmaA + m_sigmaS)
+    {
+
+    }
+
+    float Tr(const nori::Point3f& x, const nori::Point3f& y) const {
+        return std::exp( -m_sigmaT * (x - y).norm() );
+    }
+
+    /*
+    - infinite
+    - homogeneous
+        Tr(x,y) = e^(-sigmaS * norm(x-y))
+    - isotropic
+        PDF = 1 / 4pi
+    - monochromatic
+    - no surfaces
+    - single scattering
+        w NEE
+        1 x area light (area sampling)
+    > in-scattering (3rd part of VRE)
+    */
+    Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray, const Intersection* _its) const {
+        if (m_tracerType == "explicit") {
+            return Li_explicit(scene, sampler, ray);
+        }
+        else { //if (m_tracerType == "implicit") {
+            return 0.0f;
+        }
+    }
+
+    Color3f Li_explicit(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+        float maxt = scene->getBoundingBox().getExtents().norm();
+        Point3f x = ray.o;
+        Vector3f wi = ray.d;
+
+        // sample distance (t)
+        float t = -std::log(1 - sampler->next1D()) / m_sigmaT;
+        float pdfT = m_sigmaT * std::exp(-m_sigmaT*t);
+
+        Intersection its;
+        if (scene->rayIntersect(ray, its) && its.shape->isEmitter()) {
+            float tmax = (x-its.p).norm();
+            if(t >= tmax) {
+                float pdf = std::exp(-m_sigmaT * tmax);
+                Color3f Le = Tr(x,its.p) * its.shape->getEmitter()->eval(its, -ray.d) / pdf;
+                return Le;
+            }
+        }
+
+        // calc point xt
+        Point3f xt = x + t*wi;
+
+        // sample light
+        Emitter* em = scene->getEmitters()[0];
+        Color3f Le_(0.0f);
+        /*
+        // - area
+        Normal3f yN;
+        Point3f xe = em->sample(sampler, yN);
+        Vector3f wo = (xe - xt).normalized();
+
+        Ray3f lightRay(xt, wo, Epsilon, maxt);
+        Intersection itsLight;
+        bool intersects = scene->rayIntersect(lightRay, itsLight);
+        if (intersects && (itsLight.shape->isEmitter())) {
+            float cosThetaY = std::max(0.0f, (-wo).dot(yN));
+            if (cosThetaY > 0.0f) { // check for division by zero
+                float pA = 1.0f / em->getShape()->getArea();
+                float d2 = (xe - xt).squaredNorm();
+                float pdf = d2 / cosThetaY * pA;
+
+                Le_ = em->eval(itsLight, wo) / pdf;
+            }
+        }
+        */
+
+        // - solidangle
+        Point3f xe; Normal3f yN; float pWi;
+        Vector3f wo = em->sampleSolidAngle(sampler, xt, yN, pWi, xe);
+
+        Ray3f lightRay(xt, wo, Epsilon, maxt);
+        Intersection itsLight;
+        bool intersects = scene->rayIntersect(lightRay, itsLight);
+        if (intersects && (itsLight.shape->isEmitter())) {
+            Le_ = itsLight.shape->getEmitter()->eval(itsLight, wo) / pWi;
+        }
+
+        // calc Li w NEE
+        Color3f Li = Tr(xt, xe) * Le_;
+
+        // calc Ls
+        float fp = 1.0f / (4.0f * M_PI);
+        Color3f Ls = fp * Li;
+
+        Color3f Lr = Tr(x, xt) * m_sigmaS * Ls / pdfT;
+        return Lr;
+    }
+
+    std::string toString() const {
+        return tfm::format(
+            "VolumePT[\n"
+            " tracerType = %s\n"
+            " termination = %s\n"
+            " terminationProb = %d\n"
+            " terminationBounds = %d\n"
+            " sigmaA = %d\n"
+            " sigmaS = %d\n"
+            " sigmaT = %d\n"
+            "]",
+            m_tracerType,
+            m_termination,
+            m_terminationProb,
+            m_terminationBounds,
+            m_sigmaA,
+            m_sigmaS,
+            m_sigmaT
+        );
+    }
+
+private:
+    const std::string m_tracerType;
+    const std::string m_termination;
+    const float m_terminationProb;
+    const int m_terminationBounds;
+    const float m_sigmaA;
+    const float m_sigmaS;
+    const float m_sigmaT;
+};
+
+NORI_REGISTER_CLASS(VolumePT, "volumePT");
+NORI_NAMESPACE_END
