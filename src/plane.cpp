5383d7c0abe311a92e7c17bb99694e5dfbad2832
Keven Villeneuve <keven.villeneuve@gmail.com>
2017-11-09 19:44:47 -0500

    PT: Implement subtended solidangle for rectangle light using spherical rectangle approx

--- src/plane.cpp
+++ src/plane.cpp
@@ -104,8 +104,111 @@ public:
         return y;
     }
 
+    // Implementation of "An Area-Preserving Parametrization for Spherical Rectangles"
+    struct SphQuad {
+        Vector3f o, x, y, z; // local reference system 'R'
+        float z0, z0sq; // rectangle coords in 'R'
+        float x0, y0, y0sq;
+        float x1, y1, y1sq;
+        float b0, b1, b0sq, k; // misc precomputed constants
+        float S; // solid angle of 'Q'
+    };
+
     virtual Vector3f sampleSolidAngle(Sampler* sampler, Point3f& x, Normal3f& normal, float& pWi, Point3f& y) const override {
-        throw NoriException("Unimplemented Plane::sampleSolidAngle() !!!");
+        // make sure rect light only emmit from one side
+        if( (x - m_center).dot(m_normalFrame.n) < 0.0f ) {
+            pWi = 0.0f;
+            return 0.0f;
+        }
+
+        Point3f s = m_objectToWorld * Point3f(-1.0f,-1.0f,0.0f);
+        Vector3f ex = m_objectToWorld * Vector3f(2.0f,0.0f,0.0f);
+        Vector3f ey = m_objectToWorld * Vector3f(0.0f,2.0f,0.0f);
+        SphQuad squad;
+        SphQuadInit(squad, s, ex, ey, x);
+        y = SphQuadSample(squad, sampler->next1D(), sampler->next1D());
+
+        Vector3f wo = (y-x).normalized();
+        pWi = 1.0f / squad.S;
+        normal = m_normalFrame.n;
+        return wo;
+    }
+
+    Point3f SphQuadSample(const SphQuad& squad, float u, float v) const {
+        // 1. compute 'cu'
+        float au = u * squad.S + squad.k;
+        float fu = (std::cos(au) * squad.b0 - squad.b1) / std::sin(au);
+        float cu = 1.0f / std::sqrt(fu*fu + squad.b0sq) * (fu > 0 ? +1.0f : -1.0f);
+        cu = clamp(cu, -1.0f, 1.0f); // avoid NaNs
+
+        // 2. compute 'xu'
+        float xu = -(cu * squad.z0) / std::sqrt(1.0f - cu*cu);
+        xu = clamp(xu, squad.x0, squad.x1); // avoid Infs
+
+        // 3. compute 'yv'
+        float d = std::sqrt(xu*xu + squad.z0sq);
+        float h0 = squad.y0 / std::sqrt(d*d + squad.y0sq);
+        float h1 = squad.y1 / std::sqrt(d*d + squad.y1sq);
+        float hv = h0 + v * (h1-h0), hv2 = hv*hv;
+        float yv = (hv2 < 1-Epsilon) ? (hv*d)/std::sqrt(1 - hv2) : squad.y1;
+
+        // 4. transform (xu,yv,z0) to world coords
+        return (squad.o + xu*squad.x + yv*squad.y + squad.z0*squad.z);
+    }
+
+    void SphQuadInit(SphQuad& squad, const Point3f& s, const Vector3f& ex, const Vector3f& ey, const Point3f& o) const {
+        squad.o = o;
+        float exl = ex.norm(), eyl = ey.norm();
+
+        // compute local reference system ’R’
+        squad.x = ex / exl;
+        squad.y = ey / eyl;
+        squad.z = squad.x.cross(squad.y);
+
+        // compute rectangle coords in local reference system
+        Vector3f d = s - o;
+        squad.z0 = d.dot(squad.z);
+
+        // flip ’z’ to make it point against ’Q’
+        if (squad.z0 > 0) {
+            squad.z *= -1;
+            squad.z0 *= -1;
+        }
+        squad.z0sq = squad.z0 * squad.z0;
+        squad.x0 = d.dot(squad.x);
+        squad.y0 = d.dot(squad.y);
+        squad.x1 = squad.x0 + exl;
+        squad.y1 = squad.y0 + eyl;
+        squad.y0sq = squad.y0 * squad.y0;
+        squad.y1sq = squad.y1 * squad.y1;
+
+        // create vectors to four vertices
+        Vector3f v00(squad.x0, squad.y0, squad.z0);
+        Vector3f v01(squad.x0, squad.y1, squad.z0);
+        Vector3f v10(squad.x1, squad.y0, squad.z0);
+        Vector3f v11(squad.x1, squad.y1, squad.z0);
+
+        // compute normals to edges
+        Vector3f n0 = v00.cross(v10).normalized();
+        Vector3f n1 = v10.cross(v11).normalized();
+        Vector3f n2 = v11.cross(v01).normalized();
+        Vector3f n3 = v01.cross(v00).normalized();
+
+        // compute internal angles (gamma_i)
+        float g0 = std::acos(-n0.dot(n1));
+        float g1 = std::acos(-n1.dot(n2));
+        float g2 = std::acos(-n2.dot(n3));
+        float g3 = std::acos(-n3.dot(n0));
+
+        // compute predefined constants
+        squad.b0 = n0.z();
+        squad.b1 = n2.z();
+        squad.b0sq = squad.b0 * squad.b0;
+        squad.k = 2*M_PI - g2 - g3;
+
+        // compute solid angle from internal angles
+        squad.S = g0 + g1 - squad.k;
+        squad.S = std::max(0.0f, squad.S); // make sure it's not negative
     }
 
     virtual bool rayIntersect(uint32_t index, const Ray3f &ray, float &u, float &v, float &t) const override {
